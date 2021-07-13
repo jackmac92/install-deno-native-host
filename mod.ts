eb821f9816bfc769c4cf5118b5495b83e02a6978
Jack McCown <jackmac79@gmail.com>
2021-07-13 19:25:38 -0400

    chore: reformat, debug async check script

--- mod.ts
+++ mod.ts
@@ -8,146 +8,169 @@ const encoder = new TextEncoder();
 const encoded = (val: string) => encoder.encode(val);
 
 const findBrowserConfigDir = async (browser: string) => {
-    const osPlatform = os.platform();
-    console.log(`Looking up dir for ${browser} on ${osPlatform}`);
-    const orderedLocationsToCheck = {
-        "linux chrome": [".config/google-chrome", ".config/google-chrome-beta"],
-        "linux chromium": [".config/chromium"],
-        "linux brave": [".config/BraveSoftware/Brave-Browser"],
-        "linux vivaldi": [".config/vivaldi"],
-        "darwin chrome": [
-            "Library/Application Support/Google/Chrome",
-            "Library/Application Support/Google/Chrome Beta",
-        ],
-        "darwin chromium": ["Library/Application Support/Chromium"],
-        "darwin vivaldi": ["Library/Application Support/Vivaldi"],
-    }[`${osPlatform} ${browser}`];
+  const osPlatform = os.platform();
+  console.log(`Looking up dir for ${browser} on ${osPlatform}`);
+  const orderedLocationsToCheck = {
+    "linux chrome": [".config/google-chrome", ".config/google-chrome-beta"],
+    "linux chromium": [".config/chromium"],
+    "linux brave": [".config/BraveSoftware/Brave-Browser"],
+    "linux vivaldi": [".config/vivaldi"],
+    "darwin chrome": [
+      "Library/Application Support/Google/Chrome",
+      "Library/Application Support/Google/Chrome Beta",
+    ],
+    "darwin chromium": ["Library/Application Support/Chromium"],
+    "darwin vivaldi": ["Library/Application Support/Vivaldi"],
+  }[`${osPlatform} ${browser}`];
 
-    if (!orderedLocationsToCheck) {
-        throw new Error("Unknown browser/os combo")
-    }
+  if (!orderedLocationsToCheck) {
+    throw new Error("Unknown browser/os combo");
+  }
 
-    for (const potentialLocation of orderedLocationsToCheck) {
-        const p = `${homepath}/${potentialLocation}`;
-        if (await exists(p)) {
-            return p;
-        }
+  for (const potentialLocation of orderedLocationsToCheck) {
+    const p = `${homepath}/${potentialLocation}`;
+    if (await exists(p)) {
+      return p;
     }
-    throw new Error("Unable to locate chrome config dir");
+  }
+  throw new Error("Unable to locate chrome config dir");
 };
 
 const ensureDirSafe = async (path: string) => {
-    const components = path.split("/");
+  const components = path.split("/");
 
-    const [_, pathAncestors] = Array(components.length).reduce(
-        (acc, el) => {
-            const [pathComponents, result] = acc;
-            const nextPathComponents = [...pathComponents];
-            const nextComponent = nextPathComponents.join("/");
-            nextPathComponents.pop();
-            if (nextComponent === homepath) {
-                return acc;
-            }
-            return [nextPathComponents, [nextComponent, ...result]];
-        },
-        [components, []]
-    );
-    for (let ancestor of pathAncestors) {
-        await ensureDir(ancestor);
-    }
+  const [_, pathAncestors] = Array(components.length).reduce(
+    (acc, el) => {
+      const [pathComponents, result] = acc;
+      const nextPathComponents = [...pathComponents];
+      const nextComponent = nextPathComponents.join("/");
+      nextPathComponents.pop();
+      if (nextComponent === homepath) {
+        return acc;
+      }
+      return [nextPathComponents, [nextComponent, ...result]];
+    },
+    [components, []]
+  );
+  for (let ancestor of pathAncestors) {
+    await ensureDir(ancestor);
+  }
 };
 
 const writeShellScript = async (
-    targetPath: string,
-    codeURI: string,
-    denoFlags: string
+  targetPath: string,
+  codeURI: string,
+  denoFlags: string
 ) => {
-    const denoCmd = Deno.execPath();
-    /// interesting idea below but just passing a string seems more straightforward
-    // const flags = denoFlags.map((f) => `--${f}`).join(" ");
-    const scriptContent = `#!/usr/bin/env bash\n${denoCmd} run ${denoFlags} ${codeURI}`;
+  const denoCmd = Deno.execPath();
+  /// interesting idea below but just passing a string seems more straightforward
+  // const flags = denoFlags.map((f) => `--${f}`).join(" ");
+  const scriptContent = `#!/usr/bin/env bash\n${denoCmd} run ${denoFlags} ${codeURI}`;
 
-    await Deno.writeFile(targetPath, encoded(scriptContent));
-    await Deno.chmod(targetPath, 0o777);
+  await Deno.writeFile(targetPath, encoded(scriptContent));
+  await Deno.chmod(targetPath, 0o777);
 };
 
 const scriptURItoConfigURI = (denoURI: string) =>
-    `${denoURI.split("/").slice(0, -1).join("/")}/native-host-params.ts`;
+  `${denoURI.split("/").slice(0, -1).join("/")}/native-host-params.ts`;
 
 yargs(Deno.args)
-    .command(
-        "install <denoURI>",
-        "install a deno native messaging server",
-        (yargs: any) => {
-            yargs.positional("denoURI", {
-                description: "The public url of the deno script to run",
-            });
-            yargs.option("autoConfig");
-            yargs.describe(
-                "autoConfig",
-                "lookup config in `native-host-params.ts` sibling file of the main URI"
-            );
+  .command(
+    "install <denoURI>",
+    "install a deno native messaging server",
+    (yargs: any) => {
+      yargs.positional("denoURI", {
+        description: "The public url of the deno script to run",
+      });
+      yargs.option("autoConfig");
+      yargs.describe(
+        "autoConfig",
+        "lookup config in `native-host-params.ts` sibling file of the main URI"
+      );
 
-            yargs.option("denoFlags");
-            yargs.describe(
-                "denoFlags",
-                "flags to pass to deno when invoking the native host"
-            );
-            yargs.default("denoFlags", "");
+      yargs.option("denoFlags");
+      yargs.describe(
+        "denoFlags",
+        "flags to pass to deno when invoking the native host"
+      );
+      yargs.default("denoFlags", "");
 
-            yargs.option("browser");
-            yargs.describe(
-                "browser",
-                "the target browser for the native host extension"
-            );
-            yargs.default("browser", "chrome");
+      yargs.option("browser");
+      yargs.describe(
+        "browser",
+        "the target browser for the native host extension"
+      );
+      yargs.default("browser", "chrome");
 
-            yargs.option("resourceId");
-            yargs.describe(
-                "resourceId",
-                "The resource id of the native messaging host"
-            );
+      yargs.option("resourceId");
+      yargs.describe(
+        "resourceId",
+        "The resource id of the native messaging host"
+      );
 
-            yargs.option("description");
+      yargs.option("description");
 
-            yargs.array("allowedOrigins");
+      yargs.array("allowedOrigins");
 
-            return yargs;
-        },
-        async (argv: Arguments) => {
-            const { denoURI, autoConfig, browser, denoFlags } = argv;
-            console.log("Starting deno native host installation");
-            const { resourceId, allowedOrigins, description } =
-                await (async () => {
-                    if (!autoConfig) {
-                        return argv;
-                    }
-                    const configUrl = scriptURItoConfigURI(denoURI);
-                    const res = await import(configUrl);
-                    return res;
-                })();
-
-            const targetPathDir = `${homepath}/.local/var/deno-native-messaging`;
-            await ensureDirSafe(targetPathDir);
-            const targetPath = `${targetPathDir}/${resourceId}.sh`;
-            await writeShellScript(targetPath, denoURI, denoFlags);
-
-            const content = {
-                name: resourceId,
-                path: targetPath,
-                description,
-                allowed_origins: allowedOrigins,
-                type: "stdio",
-            };
-
-            const chromeDir = await findBrowserConfigDir(browser);
-            const nativeMessagingHostJsonPath = `${chromeDir}/NativeMessagingHosts/${resourceId}.json`;
-            Deno.writeFile(
-                nativeMessagingHostJsonPath,
-                encoded(JSON.stringify(content))
-            );
-            console.log("Install success!");
+      return yargs;
+    },
+    async (argv: Arguments) => {
+      const { denoURI, autoConfig, browser, denoFlags } = argv;
+      console.log("Starting deno native host installation");
+      const { resourceId, allowedOrigins, description } = await (async () => {
+        if (!autoConfig) {
+          return argv;
         }
-    )
-    // @ts-expect-error
-    .strictCommands().argv;
+        const configUrl = scriptURItoConfigURI(denoURI);
+        const res = await import(configUrl);
+        return res;
+      })();
+
+      const targetPathDir = `${homepath}/.local/var/deno-native-messaging`;
+      await ensureDirSafe(targetPathDir);
+      const targetPath = `${targetPathDir}/${resourceId}.sh`;
+      await writeShellScript(targetPath, denoURI, denoFlags);
+
+      const content = {
+        name: resourceId,
+        path: targetPath,
+        description,
+        allowed_origins: allowedOrigins,
+        type: "stdio",
+      };
+
+      const chromeDir = await findBrowserConfigDir(browser);
+      const nativeMessagingHostJsonPath = `${chromeDir}/NativeMessagingHosts/${resourceId}.json`;
+      Deno.writeFile(
+        nativeMessagingHostJsonPath,
+        encoded(JSON.stringify(content))
+      );
+      console.log("Install success!");
+    }
+  )
+  // .check( /* async */ (argv: Arguments) => {
+  //   const { resourceId, autoConfig, denoURI, allowedOrigins, description } =
+  //     argv;
+  //   await import(denoURI);
+  //   if (autoConfig) {
+  //     await import(scriptURItoConfigURI(denoURI));
+  //     // if the above resolves ignore remaining tests
+  //     return;
+  //   }
+  //   allowedOrigins.forEach((o: string) => {
+  //     if (!o.startsWith("chrome-extension://")) {
+  //       throw new Error(
+  //         "Invalid allowed origin, all must start with chrome-extension://"
+  //       );
+  //     }
+  //   });
+  //   if (resourceId.length === 0) {
+  //     throw new Error("Provide a resourceId");
+  //   }
+  //   if (description.length === 0) {
+  //     throw new Error("Provide a description");
+  //   }
+  //   console.log("Validated native host!");
+  // })
+  // @ts-expect-error
+  .strictCommands().argv;
